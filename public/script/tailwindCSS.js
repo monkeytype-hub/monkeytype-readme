857123e0084c7636e166ee836e1ba3e8ef17ddbd
ridemountainpig <lin0905580802@gmail.com>
2023-04-07 13:34:49 +0800

    feat: add get tailwind css function

---
+++ public/script/tailwindCSS.js
@@ -0,0 +1,18 @@
+const util = require('util');
+const fs = require('fs');
+const readFile = util.promisify(fs.readFile);
+
+async function getOutputCSS() {
+    try {
+        const data = await readFile('./dist/output.css', 'utf-8');
+        return data;
+    } catch (err) {
+        console.error(err);
+        return;
+    }
+}
+
+
+module.exports = {
+    getOutputCSS
+};
