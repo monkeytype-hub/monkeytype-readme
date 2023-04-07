88d0e845dd9582cad29e18b908035e4d5f783656
ridemountainpig <lin0905580802@gmail.com>
2023-04-08 00:22:33 +0800

    refactor: change get user data and svg to function

--- app.js
+++ app.js
@@ -1,52 +1,23 @@
 const express = require('express');
 const app = express();
 
-const axios = require('axios');
-const { getTheme, getBadge } = require('./public/script/monkeytypeData');
+const { getTheme, getBadge, getUserData } = require('./public/script/monkeytypeData');
 const { getOutputCSS } = require('./public/script/tailwindCSS');
+const { getSvg } = require('./public/script/generateSvg');
 require('dotenv').config();
 
 app.get('/', (req, res) => {
     res.send('MonkeyType README!');
 });
 
-app.get('/generate-svg/:userId', async (req, res) => {
+app.get('/generate-svg/:userId/:themeName', async (req, res) => {
     const userId = req.params.userId;
-
-    const API_KEY = process.env.MONKEYTYPE_APEKEY;
-    const url = `https://api.monkeytype.com/users/${userId}/profile`;
-
-    const width = 500;
-    const height = 200;
-    const cssData = await getOutputCSS();
-    console.log(cssData)
-
-    axios.get(url, {
-        headers: {
-            'Authorization': `ApeKey ${API_KEY}`
-        },
-    })
-        .then((response) => {
-            const userData = response.data.data;
-            const svg = `
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${width} ${height}" width="${width}" height="${height}">
-                    <style>
-                        ${cssData}
-                    </style>
-                    <rect x="0" y="0" width="${width}" height="${height}"/>
-                    <foreignObject x="0" y="0" width="${width}" height="${height}" class="bg-white">
-                        <div xmlns="http://www.w3.org/1999/xhtml">
-                            <h1 class="font-extrabold text-yellow-400">Hello MonkeyType README !!!</h1>
-                        </div>
-                    </foreignObject>
-                </svg>
-            `;
-            res.set('Content-Type', 'image/svg+xml');
-            res.send(svg);
-        })
-        .catch((error) => {
-            console.error(error);
-        });
+    const themeName = req.params.themeName;
+    const userData = await getUserData(userId);
+    const theme = getTheme(themeName);
+    const svg = await getSvg(userData, theme);
+    res.set('Content-Type', 'image/svg+xml');
+    res.send(svg);
 });
 
 app.listen(3000, () => {
