70b2ae742a986747e24081b9882ce33d6c356335
ridemountainpig <lin0905580802@gmail.com>
2023-04-12 00:04:23 +0800

    fix: user discord without img will cause error

--- public/script/generateSvg.js
+++ public/script/generateSvg.js
@@ -20,14 +20,15 @@ async function getSvg(userData, theme, badge) {
     const cssData = await getOutputCSS();
 
     let userImg;
-    if (userData.discordId === null || userData.discordAvatar === undefined) {
-        userImg = `
-            <div class="w-20 h-20 rounded-full">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="${theme.subColor}">
-                    <path d="M399 384.2C376.9 345.8 335.4 320 288 320H224c-47.4 0-88.9 25.8-111 64.2c35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 16a72 72 0 1 0 0-144 72 72 0 1 0 0 144z"/>
-                </svg>
-            </div>
-        `;
+    let defaultUserImg = `
+        <div class="w-20 h-20 rounded-full">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="${theme.subColor}">
+                <path d="M399 384.2C376.9 345.8 335.4 320 288 320H224c-47.4 0-88.9 25.8-111 64.2c35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 16a72 72 0 1 0 0-144 72 72 0 1 0 0 144z"/>
+            </svg>
+        </div>
+    `
+    if (userData.discordId === undefined || userData.discordAvatar === undefined) {
+        userImg = defaultUserImg;
     } else {
         // Download the image and save it to a file
         const imagePath = `public/image/userImg/${userData.discordId}-${userData.discordAvatar}.png`;
@@ -37,11 +38,15 @@ async function getSvg(userData, theme, badge) {
         const imageData = fs.readFileSync(imagePath);
         const base64Image = imageData.toString('base64');
 
-        userImg = `
-            <div class="w-20 h-20 rounded-full overflow-hidden">
-                <img src="data:image/png;base64,${base64Image}" width="80" height="80"/>
-            </div>
-        `;
+        if (base64Image == "") {
+            userImg = defaultUserImg;
+        } else {
+            userImg = `
+                <div class="w-20 h-20 rounded-full overflow-hidden">
+                    <img src="data:image/png;base64,${base64Image}" width="80" height="80"/>
+                </div>
+            `;
+        }
     }
 
     let userBadge = "";
